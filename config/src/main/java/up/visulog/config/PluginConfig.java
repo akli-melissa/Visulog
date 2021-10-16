1a236b9637e9fa59725eec4f9d8bab718bb75e8d
SALHI younes <younessalhi520@gmail.com>
2021-10-16 21:53:22 +0200

    Update PluginConfig.java

--- config/src/main/java/up/visulog/config/PluginConfig.java
+++ config/src/main/java/up/visulog/config/PluginConfig.java
@@ -5,3 +5,5 @@ package up.visulog.config; //import du fichier config.java du projet
 // TODO: define what this type should be (probably a Map: settingKey -> settingValue)
 public interface PluginConfig {
 }
+
+//j'accepte le merge
