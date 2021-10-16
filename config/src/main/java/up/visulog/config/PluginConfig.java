31a330a72d02527da6ce943b1227658c11f365d8
Sofiane Boudet <Sofiane.boudet1@gmail.com>
2021-10-16 15:24:30 +0200

    reviewed the class PluginConfig.java

--- config/src/main/java/up/visulog/config/PluginConfig.java
+++ config/src/main/java/up/visulog/config/PluginConfig.java
@@ -1,4 +1,6 @@
-package up.visulog.config;
+package up.visulog.config; //import du fichier config.java du projet
+
+// Cette classe accueillera les config des plugins pour l interface.
 
 // TODO: define what this type should be (probably a Map: settingKey -> settingValue)
 public interface PluginConfig {
