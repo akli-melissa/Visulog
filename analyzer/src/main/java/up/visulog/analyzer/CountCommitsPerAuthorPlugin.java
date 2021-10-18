e4827762a2dedad52cfa779736ea014fea8f7875
Salhi Younes <younessalhi520@gmail.com>
2021-10-18 14:58:47 +0200

    changes

--- analyzer/src/main/java/up/visulog/analyzer/CountCommitsPerAuthorPlugin.java
+++ analyzer/src/main/java/up/visulog/analyzer/CountCommitsPerAuthorPlugin.java
@@ -2,7 +2,6 @@ package up.visulog.analyzer;
 
 import up.visulog.config.Configuration;
 import up.visulog.gitrawdata.Commit;
-
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
