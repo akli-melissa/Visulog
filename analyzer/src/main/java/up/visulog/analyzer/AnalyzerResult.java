0aeb654500c316d5f235c784e56d374970e3a304
yahmiy <yanis.yahmi2002@gmail.com>
2021-11-08 13:48:14 +0100

    Ouverture du fichier html des résultats dans le navigateur

--- analyzer/src/main/java/up/visulog/analyzer/AnalyzerResult.java
+++ analyzer/src/main/java/up/visulog/analyzer/AnalyzerResult.java
@@ -19,6 +19,32 @@ public class AnalyzerResult {
     }
 
     public String toHTML() {
-        return "<html><body>"+subResults.stream().map(AnalyzerPlugin.Result::getResultAsHtmlDiv).reduce("", (acc, cur) -> acc + cur) + "</body></html>";
+//-----------------------------
+      /*  System.out.println(new File("").getAbsolutePath());
+                String html_code = "";
+                html_code += Files.readString(Path.of("../webgen/src/main/java/up/visulog/webgen/Pages/basicPage_begining.html"));
+                if(subResults.size() != 0) {
+                    for(AnalyzerPlugin.Result result : subResults) {
+                        html_code += "<h1 class=\"text-center\">"+result.getDisplayName()+"</h1>";
+                        Webgen.Graph[] graphArray = result.getResultAsGraphArray();
+                        html_code += "<div class=\"container\"><div class=\"row\">";
+                        int c = 0;
+                        for(Webgen.Graph graph : graphArray) {
+                            if(c == 2) {
+                                html_code += "</div><div class=\"row\">";
+                                c = 0;
+                            }
+                            html_code += "<div class=\"col-4\"><p class=\"canvas-paragraph\">" + graph.getHTML() + "</p></div>";
+                        }
+                        html_code += "</div></div>";
+                    }
+
+                } else {
+                    html_code += "<h2> Aucun plugin sélectionné! </h2>";
+                }
+                html_code += Files.readString(Path.of("../webgen/src/main/java/up/visulog/webgen/Pages/basicPage_end.html"));
+                return html_code;*/
+                 return "<html><body>"+subResults.stream().map(AnalyzerPlugin.Result::getResultAsHtmlDiv).reduce("", (acc, cur) -> acc + cur) + "</body></html>";
+        //---------------------------------
     }
 }
