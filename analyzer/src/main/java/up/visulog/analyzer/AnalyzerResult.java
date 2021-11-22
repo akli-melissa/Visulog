999f79824ce60ce4147ac1107bc0ca0ef07b0480
yahmiy <yanis.yahmi2002@gmail.com>
2021-11-22 20:42:53 +0100

    Graphe circulaire: Commits

--- analyzer/src/main/java/up/visulog/analyzer/AnalyzerResult.java
+++ analyzer/src/main/java/up/visulog/analyzer/AnalyzerResult.java
@@ -19,6 +19,23 @@ public class AnalyzerResult {
     }
 
     public String toHTML() {
-        return "<html><body>"+subResults.stream().map(AnalyzerPlugin.Result::getResultAsHtmlDiv).reduce("", (acc, cur) -> acc +"\n"+ cur) + "</body></html>";
+        StringBuilder html = new StringBuilder("");
+        html.append("<!DOCTYPE html>");
+        html.append("<html lang=\"en\">");
+        html.append("<head>");
+        html.append("<script src=\"https://cdnjs.cloudflare.com/ajax/libs/Chart.js/3.6.0/chart.min.js\" integrity=\"sha512-GMGzUEevhWh8Tc/njS0bDpwgxdCJLQBWG3Z2Ct+JGOpVnEmjvNx6ts4v6A2XJf1HOrtOsfhv3hBKpK9kE5z8AQ==\" crossorigin=\"anonymous\" referrerpolicy=\"no-referrer\"></script>");
+        html.append("<meta charset=\"utf-8\">");
+        html.append("<meta name=\"viewport\" content=\"width=device-width, initial-scale=1, shrink-to-fit=no\">");
+        html.append("<title>Visulog Statistics</title>");
+        html.append("</head>");
+        html.append(subResults.stream().map(AnalyzerPlugin.Result::getResultAsHtmlDiv).reduce("", (acc, cur) -> acc +"\n"+ cur));
+        html.append("</body>");
+        html.append("</html>");
+        //html.append("");
+        //html.append("");
+        //html.append("");
+        //html.append("");
+        return(html.toString());
+        //return "<html><body>"+subResults.stream().map(AnalyzerPlugin.Result::getResultAsHtmlDiv).reduce("", (acc, cur) -> acc +"\n"+ cur) + "</body></html>";
     }
 }
--- analyzer/src/main/java/up/visulog/analyzer/CountCommitsPerAuthorPlugin.java
+++ analyzer/src/main/java/up/visulog/analyzer/CountCommitsPerAuthorPlugin.java
@@ -6,6 +6,12 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.util.Arrays;
+
 public class CountCommitsPerAuthorPlugin implements AnalyzerPlugin {
     private final Configuration configuration;
     private Result result;// classe interne
@@ -52,14 +58,63 @@ public class CountCommitsPerAuthorPlugin implements AnalyzerPlugin {
 
         @Override
         public String getResultAsHtmlDiv() {
+            StringBuilder builder = new StringBuilder("");
+            builder.append("<canvas id=\"graph\"></canvas>");
+            builder.append("<script src=\"../countCommits.js\"></script>");
+            builder.append("<script>countCommits();</script>");
+            String s = "graph";
+            this.parseJS(s);
+            return builder.toString();
+        }
 
             // HtmlFlow -> A ajouter
-            StringBuilder html = new StringBuilder("<div>Commits per author: <ul>");
-            for (var item : commitsPerAuthor.entrySet()) {
-                html.append("<li>").append(item.getKey()).append(": ").append(item.getValue()).append("</li>");
+
+        
+    
+
+
+
+
+         private String formatArrayString(String[] data) {
+            String result = Arrays.asList(data).stream().reduce("", (a, b) -> "\"" + a + ",\"" + b + "\"");
+            return "[" + result.substring(result.indexOf(",") + 1, result.length()) + "]";
+        }
+
+        private void parseJS(String id2){
+            String[] names = new String[this.commitsPerAuthor.size()];
+            String[] Nbcommits = new String[this.commitsPerAuthor.size()];
+            int i = 0;
+            
+            for (Map.Entry<String, Integer> entry :this.commitsPerAuthor.entrySet()){
+                names[i] = entry.getKey();
+                Nbcommits[i] =  String.valueOf(entry.getValue());
+                i++;
             }
-            html.append("</ul></div>");
-            return html.toString();
+
+            StringBuilder data = new StringBuilder("function countCommits(){");
+            data.append("const graph = document.getElementById('"+id2+"').getContext('2d');");
+            data.append("let myChart = new Chart(graph, {");
+            data.append("type:\"pie\",");
+            data.append("data: {");
+            data.append("labels:" + formatArrayString(names) + ",");
+            data.append("datasets: [{");
+            data.append("label:\"Nombre de commits par personnes\",");
+            data.append("data:" + formatArrayString(Nbcommits) + ",");
+            data.append("backgroundColor:['#ED7F10','#FFCB60','#F1E2BE', '#F4661B','#B36700','#FEA347','#F3D617','#FF7F00','#E1CE9A'],");
+            data.append(" hoverBorderWidth: 3,");
+            data.append("}],");
+            data.append("}");
+            data.append("});}");
+
+            try{
+                File f = new File("..");
+                String path = f.getAbsoluteFile()+"/countCommits.js";
+                BufferedWriter bw = new BufferedWriter(new FileWriter(path));
+                bw.write(data.toString());
+                bw.close();
+            }catch(Exception e){
+                e.printStackTrace();
+            }
         }
     }
 }
