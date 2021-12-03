a618a00968b6003eaaf43dd0744512187fae1c2f
Salhi Younes <younessalhi520@gmail.com>
2021-12-03 16:56:25 +0100

    MAJ de la casse CountLinesPerAuthor.java

--- analyzer/src/main/java/up/visulog/analyzer/CountLinesPerAuthor.java
+++ analyzer/src/main/java/up/visulog/analyzer/CountLinesPerAuthor.java
@@ -3,7 +3,10 @@ package up.visulog.analyzer;
 import java.util.Map;
 import java.util.HashMap;
 import java.util.List;
+import java.io.BufferedReader;
+import java.io.File;
 import java.util.ArrayList;
+import java.io.FileReader;
 import java.util.Arrays;
 import up.visulog.config.Configuration;
 import up.visulog.gitrawdata.Commit;
@@ -19,12 +22,21 @@ public class CountLinesPerAuthor implements AnalyzerPlugin{
 
     //retourne le nombre de lignes ajoutées et supprimées par le commit
     private int[] formatCommit(Commit commit){
-        int[] result = new int[2];
+        int[] result = {0,0};
         String infoCommits = commit.commitInformations;
         if (infoCommits != null && infoCommits.length() > 0){
             String[] splitedInformations = infoCommits.split(",");
-            result[0] = Integer.parseInt(splitedInformations[1].split(" ")[1]);//lignes ajoutées
-            result[1] = Integer.parseInt(splitedInformations[2].split(" ")[1]);//lignes supprimées
+            if (splitedInformations.length == 3){
+                result[0] = Integer.parseInt(splitedInformations[1].split(" ")[1]);//lignes ajoutées
+                result[1] = Integer.parseInt(splitedInformations[2].split(" ")[1]);//lignes supprimées
+            }else{
+                //juste des insertions ou des suppressions
+                if (infoCommits.contains("+")){
+                    result[0] = Integer.parseInt(splitedInformations[1].split(" ")[1]);//lignes ajoutées
+                }else{
+                    result[1] = Integer.parseInt(splitedInformations[1].split(" ")[1]);//lignes supprimées   
+                }
+            }
         }
         return result;
     }
@@ -33,13 +45,15 @@ public class CountLinesPerAuthor implements AnalyzerPlugin{
     private Result processDiff(List<Commit> commits){
         Result result = new Result();
         int[] linesAddandDel;
+        String authorName = "";
         ArrayList<Integer> linesAddDel = null;
         for (Commit commit : commits){
+            authorName = commit.author.substring(0, commit.author.indexOf("<"));
             linesAddandDel = this.formatCommit(commit);
-            linesAddDel = result.userLines.getOrDefault(commit.author, new ArrayList<Integer>(Arrays.asList(0, 0)));
+            linesAddDel = result.userLines.getOrDefault(authorName, new ArrayList<Integer>(Arrays.asList(0, 0)));
             linesAddDel.set(0 ,linesAddDel.get(0) + linesAddandDel[0]);
             linesAddDel.set(1 ,linesAddDel.get(1) + linesAddandDel[1]);
-            result.userLines.put( commit.author, linesAddDel );
+            result.userLines.put( authorName, linesAddDel );
         }
         return result;
     }
@@ -71,12 +85,34 @@ public class CountLinesPerAuthor implements AnalyzerPlugin{
 
         @Override
         public String getResultAsHtmlDiv() {
-            StringBuilder html = new StringBuilder("<div>Commits per author: <ul>");
-            for (var item : userLines.entrySet()) {
-                html.append("<li>").append(item.getKey()).append(": ").append(item.getValue().get(0)).append(";"+item.getValue().get(1)).append("</li>");
+            String path = (new File(System.getProperty("user.dir"))).getParentFile() + "/webgen/Graph.html";
+            StringBuilder html = new StringBuilder("");
+            try{
+                BufferedReader in = new BufferedReader(new FileReader(path));
+                String str = "";
+                while ((str = in.readLine()) != null) {
+                    html.append(str+"\n");
+                }
+                in.close();
+            }catch(Exception e){
+                e.printStackTrace();
             }
-            html.append("</ul></div>");
-            return html.toString();
+            String datapointsA = "",datapointsB="";
+            String userName = "";
+            for (Map.Entry<String,ArrayList<Integer>> entry : userLines.entrySet()){
+                userName = entry.getKey();
+                datapointsA+="{y:"+ entry.getValue().get(0) + " ,label: \'"+userName+"\'},";
+                datapointsB+="{y:"+ entry.getValue().get(1) + " ,label: \'"+userName+"\'},";
+            }
+            String graph2 = html.toString();
+            String graph1 = html.toString();
+
+            graph1 = graph2.replace("///data///",datapointsA).replace("_id","4")
+                    .replace("Commits","Lines Added Per User").replace("commits","lines");
+            graph2 = graph2.replace("///data///",datapointsB).replace("_id","5")
+                    .replace("Commits","Lines Deleted Per User").replace("commits","lines");
+
+            return graph1 + graph2;
         }
 
     }
