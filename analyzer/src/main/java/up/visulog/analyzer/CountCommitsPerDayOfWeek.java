bcdddf0a2a207c88ed69cdd3315b2c9ba6cd90c1
djeghali <nadinedjeghali@hotmail.com>
2021-12-05 02:02:59 +0100

    Calcul de la moyenne d'activité par jour de semaine

--- analyzer/src/main/java/up/visulog/analyzer/CountCommitsPerDayOfWeek.java
+++ analyzer/src/main/java/up/visulog/analyzer/CountCommitsPerDayOfWeek.java
@@ -17,10 +17,25 @@ public class CountCommitsPerDayOfWeek implements AnalyzerPlugin {
 
     static Result processLog(List<Commit> gitLog) {
         var result = new Result();
-        for (var commit : gitLog) {
-            String day =commit.date.split(" ")[0];
-            var nb = result.commitsPerDayOfWeek.getOrDefault(day, 0);
-            result.commitsPerDayOfWeek.put(day, nb + 1);
+
+       for (Map.Entry<String,Integer> item:result.commitsPerDayOfWeek.entrySet()){
+            Map<String, Integer> commitsPerDay = new HashMap<>();// pour chaque dimanche precis(Mon 12/20 Jan)
+
+            for (var commit : gitLog) {
+                String [] dateTable=commit.date.split(" ");
+                String day =dateTable[0]+dateTable[1]+dateTable[2]+dateTable[4];
+                if(day.contains(item.getKey())){
+                    var nb = commitsPerDay.getOrDefault(day, 0);
+                    commitsPerDay.put(day, nb + 1);
+                } 
+            }
+            if(commitsPerDay.size()!=0){
+                int sum=0;
+                for(int val:commitsPerDay.values()){
+                    sum+=val;
+                }
+               result.commitsPerDayOfWeek.put(item.getKey(),sum/commitsPerDay.size());
+            }
         }
         return result;
     }
@@ -41,8 +56,19 @@ public class CountCommitsPerDayOfWeek implements AnalyzerPlugin {
 
     // Implementation de la sous interface Result de AnalyzerPlugin
     static class Result implements AnalyzerPlugin.Result {
-        private final Map<String, Integer> commitsPerDayOfWeek = new HashMap<>();// pour chaque user un nombre de commits
+        private final Map<String, Integer> commitsPerDayOfWeek ;// pour chaque user un nombre de commits
 
+        Result(){
+            commitsPerDayOfWeek = new HashMap<>();
+            commitsPerDayOfWeek.put("Sun",0);
+            commitsPerDayOfWeek.put("Mon",0);
+            commitsPerDayOfWeek.put("Tue",0);
+            commitsPerDayOfWeek.put("Wed",0);
+            commitsPerDayOfWeek.put("Thu",0);
+            commitsPerDayOfWeek.put("Fri",0);
+            commitsPerDayOfWeek.put("Sat",0);
+
+        }
         Map<String, Integer> getcommitsPerDayOfWeek() {
             return commitsPerDayOfWeek;
         }
