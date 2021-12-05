ac033edccb31ff1ffa0b65a7ac0893d5837157c7
Anyes TAFOUGHALT <tafoughaltanyes@gmail.com>
2021-12-05 12:36:12 +0100

    Modification de la classe CountCommitPerAuthorPlugin afin de traiter lecas qui permet de calculer le nombre de commits entre 2 dates

--- analyzer/src/main/java/up/visulog/analyzer/CountCommitsPerAuthorPlugin.java
+++ analyzer/src/main/java/up/visulog/analyzer/CountCommitsPerAuthorPlugin.java
@@ -2,26 +2,45 @@ package up.visulog.analyzer;
 
 import up.visulog.config.Configuration;
 import up.visulog.gitrawdata.Commit;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+import java.text.SimpleDateFormat; 
 
 public class CountCommitsPerAuthorPlugin implements AnalyzerPlugin {
+    private String dateDebut;
+    private String dateFin;
+    static private Date debut=null;
+    static private Date fin=null;
     private final Configuration configuration;
     private Result result;// classe interne
 
     public CountCommitsPerAuthorPlugin(Configuration generalConfiguration) {
         this.configuration = generalConfiguration;
     }
-
-    static Result processLog(List<Commit> gitLog) {
+    public CountCommitsPerAuthorPlugin(Configuration generalConfiguration, String dateDebut , String dateFin)throws Exception {
+        this.configuration = generalConfiguration;
+        this.dateDebut = dateDebut; 
+        debut = new SimpleDateFormat("dd-MM-yyyy HH:mm:ss",Locale.ENGLISH).parse(dateDebut+" 00:00:00");
+        this.dateFin = dateFin ;
+        fin = new SimpleDateFormat("dd-MM-yyyy HH:mm:ss",Locale.ENGLISH).parse(dateFin+" 23:59:59");
+    }
+    static Result processLog(List<Commit> gitLog) throws Exception {
         var result = new Result();
         for (var commit : gitLog) {
-            var nb = result.commitsPerAuthor.getOrDefault(commit.author, 0);
-            result.commitsPerAuthor.put(commit.author, nb + 1);
+            if(debut == null && fin == null){
+                var nb = result.commitsPerAuthor.getOrDefault(commit.author, 0);
+                result.commitsPerAuthor.put(commit.author, nb + 1);
+            }
+            else{
+                String sDate =commit.date;
+                Date date = new SimpleDateFormat("EEE MMM d HH:mm:ss yyyy z",Locale.ENGLISH).parse(sDate); 
+                if(date.compareTo(debut) >= 0 && date.compareTo(fin) <= 0){
+                    var nb = result.commitsPerAuthor.getOrDefault(commit.author, 0);
+                    result.commitsPerAuthor.put(commit.author, nb + 1);
+                 }
+            }
         }
-        return result;
-    }
+    return result;
+}
 
     @Override
     public void run() {
