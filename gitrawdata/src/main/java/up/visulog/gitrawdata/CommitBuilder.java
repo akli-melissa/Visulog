61f4d9c5a9e5538f014382b83784ca311a32f944
Anyes TAFOUGHALT <tafoughaltanyes@gmail.com>
2022-01-10 18:15:45 +0100

    changement du champ date dans la classe CommitBuilder.java

--- gitrawdata/src/main/java/up/visulog/gitrawdata/CommitBuilder.java
+++ gitrawdata/src/main/java/up/visulog/gitrawdata/CommitBuilder.java
@@ -1,8 +1,9 @@
 package up.visulog.gitrawdata;
+import java.util.*;
 public class CommitBuilder {
     private final String id;
     private String author;
-    private String date;
+    private Date date;
     private String description;
     private String mergedFrom;
     private String commitInformations;
@@ -16,7 +17,49 @@ public class CommitBuilder {
         return this;
     }
 
-    public CommitBuilder setDate(String date) {
+    public CommitBuilder setDate(Date date) {
+        this.date = date;
+        return this;
+    }
+
+    public CommitBuilder setDescription(String description) {
+        this.description = description;
+        return this;
+    }
+
+    public CommitBuilder setMergedFrom(String mergedFrom) {
+        this.mergedFrom = mergedFrom;
+        return this;
+    }
+
+    public CommitBuilder setCommitInformations(String commitInformations){
+        this.commitInformations = commitInformations;
+        return this;
+    }
+
+    public Commit createCommit() {
+        return new Commit(id, author, date, description, mergedFrom, commitInformations);
+    }
+package up.visulog.gitrawdata;
+import java.util.*;
+public class CommitBuilder {
+    private final String id;
+    private String author;
+    private Date date;
+    private String description;
+    private String mergedFrom;
+    private String commitInformations;
+
+    public CommitBuilder(String id) {
+        this.id = id;
+    }
+
+    public CommitBuilder setAuthor(String author) {
+        this.author = author;
+        return this;
+    }
+
+    public CommitBuilder setDate(Date date) {
         this.date = date;
         return this;
     }
