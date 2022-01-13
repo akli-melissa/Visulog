206e522c80e96fcd103a841f95f0c5a855815599
Anyes TAFOUGHALT <tafoughaltanyes@gmail.com>
2022-01-13 15:46:04 +0100

    TestCountCommitPerAuthorPlugin

--- analyzer/src/test/java/up/visulog/analyzer/TestCountCommitsPerAuthorPlugin.java
+++ analyzer/src/test/java/up/visulog/analyzer/TestCountCommitsPerAuthorPlugin.java
@@ -1,5 +1,5 @@
 package up.visulog.analyzer;
-/*
+
 import org.junit.Test;
 import up.visulog.gitrawdata.Commit;
 import up.visulog.gitrawdata.CommitBuilder;
@@ -7,11 +7,11 @@ import up.visulog.gitrawdata.CommitBuilder;
 import java.util.ArrayList;
 
 import static org.junit.Assert.assertEquals;
-*/
+
 public class TestCountCommitsPerAuthorPlugin {
     /* Let's check whether the number of authors is preserved and that the sum of the commits of each author is equal to the total number of commits */
-   /* @Test
-    public void checkCommitSum() {
+    @Test
+    public void checkCommitSum() throws Exception {
         var log = new ArrayList<Commit>();
         String[] authors = {"foo", "bar", "baz"};
         var entries = 20;
@@ -23,5 +23,5 @@ public class TestCountCommitsPerAuthorPlugin {
         var sum = res.getCommitsPerAuthor().values()
                 .stream().reduce(0, Integer::sum);
         assertEquals(entries, sum.longValue());
-    }*/
+    }
 }
